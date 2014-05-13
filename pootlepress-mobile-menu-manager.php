9fa03bce481e7e517d0e1cec183a0b334964c2a9
Nick Burne <nickburne@users.noreply.github.com>
2014-05-13 10:00:59 +0100

    Update pootlepress-mobile-menu-manager.php

--- pootlepress-mobile-menu-manager.php
+++ pootlepress-mobile-menu-manager.php
@@ -3,7 +3,7 @@
 Plugin Name: Canvas Extension - Mobile Menu Manager
 Plugin URI: http://pootlepress.com/
 Description: An extension for WooThemes Canvas that allow you to customize mobile menu.
-Version: 1.0.2
+Version: 1.0.3
 Author: PootlePress
 Author URI: http://pootlepress.com/
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -16,6 +16,6 @@ require_once( 'pootlepress-mobile-menu-manager-functions.php' );
 require_once( 'classes/class-pootlepress-mobile-menu-manager.php' );
 
 $GLOBALS['pootlepress_mobile_menu_manager'] = new Pootlepress_Mobile_Menu_manager( __FILE__ );
-$GLOBALS['pootlepress_mobile_menu_manager']->version = '1.0.2';
+$GLOBALS['pootlepress_mobile_menu_manager']->version = '1.0.3';
 
 ?>
